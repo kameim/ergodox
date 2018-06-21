ce7fc8b64578900e5dc0a80081f9a31c56b92339
kameim <kamei00000000@icloud.com>
2018-06-21 13:03:22 +0900

    fix

--- ergodox_ez_keymap.c
+++ ergodox_ez_keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | EISU |           | KANA |   N  |   M  | ,  < | .  > | /  ? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Ctl/Esc| Alt |  L1  |F13/Ct|F14/FN|                                       | Up   | Down | [  { | ]  } | L2 \ |
+ *   |Ctl/Esc| Alt |  L1  | F13  | F14  |                                       | Up   | Down | [  { | ]  } | L2 \ |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Left | Right|       | Left | Right  |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
         KC_LGUI,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG2,
-        CTL_T(KC_ESC), KC_LALT,    LT(SYMB,KC_NO),  CTL_T(KC_F13),LT(FN, KC_F14),
+        CTL_T(KC_ESC), KC_LALT,    LT(SYMB,KC_NO),  KC_F13,  KC_F14,
                                                        KC_LEFT,  KC_RIGHT,
                                                               KC_PGUP,
                                                KC_SPC,KC_BSPC, KC_PGDN,
