2ef2658fd5506ffe26b793f2767b62899f5ae332
kameim <kamei00000000@icloud.com>
2018-06-21 12:43:19 +0900

    add F13/14

--- ergodox_ez_keymap.c
+++ ergodox_ez_keymap.c
@@ -18,14 +18,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  | EISU |           | KANA |   N  |   M  | ,  < | .  > | /  ? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Ctl/Esc| Alt |  L1  | Left | Right|                                       | Up   | Down | [  { | ]  } | L2 \ |
+ *   |Ctl/Esc| Alt |  L1  |F13/Ct|F14/FN|                                       | Up   | Down | [  { | ]  } | L2 \ |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | Left | Right|       | Left | Right  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | ---  |       |  Up  |        |      |
+ *                                 |      |      | PgUp |       |  Up  |        |      |
  *                                 | Space| BkSp |------|       |------|  BkSp  |Enter |
- *                                 |      |      |  N   |       | Down |        |      |
+ *                                 |      |      | PgDn |       | Down |        |      |
  *                                 `--------------------'       `----------------------'
  *  N ... For thunderbird
  *  : ... In case ErgoDox EZ is detected as JIS keyboard
@@ -39,10 +39,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
         KC_LGUI,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,       KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LANG2,
-        CTL_T(KC_ESC), KC_LALT,    LT(SYMB,KC_NO),  KC_LEFT,KC_RIGHT,
+        CTL_T(KC_ESC), KC_LALT,    LT(SYMB,KC_NO),  CTL_T(KC_F13),LT(FN, KC_F14),
                                                        KC_LEFT,  KC_RIGHT,
-                                                              KC_HOME,
-                                               KC_SPC,KC_BSPC, KC_N,
+                                                              KC_PGUP,
+                                               KC_SPC,KC_BSPC, KC_PGDN,
         // right hand
              KC_BSLS,     KC_6,   KC_7,    KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_QUOT,     KC_Y,   KC_U,    KC_I,   KC_O,   KC_P,             KC_BSLS,
